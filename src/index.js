e4799da16157d80e3278ead77cd52962e441dcbf
Mlungisi Mbanjwa <mlumbanjwacpp@gmail.com>
2025-08-25 22:32:19 +0200

    deploying

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HashRouter } from 'react-router-dom';
 import App from './App';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
-  <HashRouter basename={process.env.PUBLIC_URL}>
+  <React.StrictMode>
     <App />
-  </HashRouter>
+  </React.StrictMode>
 );
+
